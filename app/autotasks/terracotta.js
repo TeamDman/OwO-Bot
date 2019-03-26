515707236e04c2a305759a2efb479fc618ff48c4
TeamDman <TeamDman9201@gmail.com>
2019-03-26 19:12:02 +0100

    Update terracotta scope

--- app/autotasks/terracotta.js
+++ app/autotasks/terracotta.js
@@ -1,6 +1,6 @@
 module.exports = async function (client) {
     client.on('message', message => {
-        if (message.channel.id !== '527215210768105481' && message.channel.id !== '123194979509207040') return;
+        if (message.guild.id !== '349201954326708226' && message.guild.id !== '123194979509207040') return;
         if (message.content.indexOf('terracotta') == -1) return;
         if (message.content.indexOf('where') == -1 && message.content.indexOf('how') == -1) return;
         message.channel.send('WhErE iS ThE TeRrAcOtTa!?');
