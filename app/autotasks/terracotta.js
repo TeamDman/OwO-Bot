bd492229cc855d2f2b5a2840028872f123cceb1c
TeamDman <TeamDman9201@gmail.com>
2019-04-14 18:05:39 +0100

    update whitelist

--- app/autotasks/terracotta.js
+++ app/autotasks/terracotta.js
@@ -1,6 +1,6 @@
 module.exports = async function (client) {
     client.on('message', message => {
-        if (message.guild.id !== '349201954326708226' && message.guild.id !== '123194979509207040') return;
+        if (message.guild.id !== '372448486723158016' && message.guild.id !== '349201954326708226' && message.guild.id !== '123194979509207040') return;
         if (message.content.indexOf('terracotta') == -1) return;
         if (message.content.indexOf('where') == -1 && message.content.indexOf('how') == -1) return;
         message.channel.send('WhErE iS ThE TeRrAcOtTa!?');
