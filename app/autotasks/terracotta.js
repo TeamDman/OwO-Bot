c7b8e72e5b611586bc8328b8cf250e23c6c6868e
TeamDman <TeamDman9201@gmail.com>
2019-04-22 06:10:53 -0400

    Clean up core branch

--- app/autotasks/terracotta.js
+++
@@ -1,9 +0,0 @@
-module.exports = async function (client) {
-    return;
-    client.on('message', message => {
-        if (message.guild.id !== '372448486723158016' && message.guild.id !== '349201954326708226' && message.guild.id !== '123194979509207040') return;
-        if (message.content.indexOf('terracotta') == -1) return;
-        if (message.content.indexOf('where') == -1 && message.content.indexOf('how') == -1) return;
-        message.channel.send('WhErE iS ThE TeRrAcOtTa!?');
-    });
-};
