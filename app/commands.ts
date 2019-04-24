68bb52edbdf55159750dd7b682b18d99af148a00
TeamDman <TeamDman9201@gmail.com>
2019-04-24 14:23:52 -0400

    Update member, role, channel arg mappers

--- app/commands.ts
+++ app/commands.ts
@@ -78,6 +78,7 @@ export async function attemptCommand(message: Message, command: Command, content
     if (args.length < params.length)
         return `Only ${args.length} arguments found, expected ${params.length}`;
     args = [...args.slice(0, params.length - 1), args.slice(params.length - 1).join(' ')];
+    args = args.map(arg => arg.trim());
     for (let i = 0; i < params.length; i++) {
         let param = params[i];
         if (param.type === 'STRING') {
