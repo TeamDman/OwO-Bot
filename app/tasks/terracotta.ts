53ac01f82b1009c301f90b2c083ec3bf9d0f5601
TeamDman <TeamDman9201@gmail.com>
2019-04-24 13:13:16 -0400

    Add terracotta facetiousness

---
+++ app/tasks/terracotta.ts
@@ -0,0 +1,44 @@
+import {Client, Message, MessageAttachment, MessageEmbed, RichEmbed} from 'discord.js';
+import {CommandResult, Task}                                         from '../index';
+import * as logger                                                   from '../logger';
+import config                                                        from '../config';
+
+const listeners =  {
+    'message': (message:Message) => {
+        if (message.author.bot) return;
+        if (message.guild.id in config['bot guild:[channel] whitelists'] && !(message.channel.id in config['bot guild:[channel] whitelists'][message.guild.id])) return;
+        if (message.content.toLowerCase().indexOf('terracotta') == -1) return;
+        if (message.content.toLowerCase().indexOf('where') == -1 && message.content.toLowerCase().indexOf('how') == -1) return;
+        message.channel.send('WhErE iS ThE TeRrAcOtTa!?');
+    },
+};
+let running:boolean = false;
+
+function start(client: Client): CommandResult {
+    if (running)
+        return new RichEmbed().setColor('ORANGE').setDescription('Terracotta task is already running.');
+    for (let key of Object.keys(listeners)) {
+        client.addListener(key, listeners[key]);
+    }
+    running = true;
+
+    return new RichEmbed().setColor('GREEN').setDescription('Terracotta task has been started.');
+}
+
+function stop(client: Client): CommandResult {
+    if (!running)
+        return new RichEmbed().setColor('ORANGE').setDescription('Terracotta task is not running.');
+    for (let key of Object.keys(listeners)) {
+        client.removeListener(key, listeners[key]);
+    }
+    running = false;
+    return new RichEmbed().setColor('GREEN').setDescription('Terracotta task has been stopped.');
+}
+
+export default {
+    name: 'Terracotta',
+    allowConcurrent: false,
+    autoStart: true,
+    start,
+    stop
+} as Task
