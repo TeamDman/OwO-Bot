409a89fbe6682862c7def8472c4b4d9fcab2df7a
TeamDman <TeamDman9201@gmail.com>
2019-05-05 22:31:50 -0400

    Improve config indexing

--- app/config.ts
+++ app/config.ts
@@ -1,3 +1,19 @@
+import {error} from './logger';
+
 const config = require('../config.json');
 
-export default config;
+// https://stackoverflow.com/a/51413778/11141271
+
+export default new Proxy(config,{
+    get(target: {}, p: string | number | symbol, receiver: any): any {
+        let result = Reflect.get(target, p, receiver);
+        if (!target.hasOwnProperty(p))
+            throw new Error(`Unknown config value '${String(p)}'.`);
+        return result;
+    },
+    set(target: {}, p: string | number | symbol, value: any, receiver: any): boolean {
+        //return Reflect.set(target, p, value, receiver);
+        error('Config properties are read-only.');
+        return false;
+    }
+});
