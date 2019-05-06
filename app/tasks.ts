fcc3610a4be6f02d0539ceb3da86c83158170d8a
TeamDman <TeamDman9201@gmail.com>
2019-05-05 23:10:32 -0400

    Create generic ListenerTask

--- app/tasks.ts
+++ app/tasks.ts
@@ -10,7 +10,7 @@ export function getTasks(): Task[] {
 
 export function refreshTasks(): void {
     taskList.length = 0;
-    let files    = require('fs').readdirSync(__dirname + '/tasks/');
+    let files       = require('fs').readdirSync(__dirname + '/tasks/');
     for (let file of files) {
         if (!file.endsWith('.js')) continue;
         delete require.cache[require.resolve(`./tasks/${file}`)];
@@ -22,7 +22,7 @@ export function init(client: Client): void {
     refreshTasks();
     taskList.filter(t => t.autoStart).forEach(task => {
         try {
-            logger.info(logger.strip(task.start(client)))
+            logger.info(logger.strip(task.start(client)));
         } catch (error) {
             logger.error(`Error running task ${task.name}: ${error}`);
         }
@@ -53,4 +53,30 @@ export async function stopTask(client: Client, identifier: string): Promise<Mess
     } catch (error) {
         return new RichEmbed().setColor('ORANGE').setDescription(error);
     }
+}
+
+export function ListenerTask(name: string, listeners: { [index: string]: (...args: any) => void }): void {
+    let running: boolean = false;
+    this.name            = name;
+    this.allowConcurrent = false;
+    this.autoStart       = true;
+    this.start           = (client: Client): MessageContent => {
+        if (running)
+            return new RichEmbed().setColor('ORANGE').setDescription(`${name} task is already running.`);
+        for (let key of Object.keys(listeners)) {
+            client.addListener(key, listeners[key]);
+        }
+        running = true;
+
+        return new RichEmbed().setColor('GREEN').setDescription(`${name} task has been started.`);
+    };
+    this.stop            = (client: Client): MessageContent => {
+        if (!running)
+            return new RichEmbed().setColor('ORANGE').setDescription(`${name} task is not running.`);
+        for (let key of Object.keys(listeners)) {
+            client.removeListener(key, listeners[key]);
+        }
+        running = false;
+        return new RichEmbed().setColor('GREEN').setDescription(`${name} task has been stopped.`);
+    };
 }
