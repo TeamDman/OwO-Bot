fa49fa1a381f70cbfe153aec2441297c65bc90fb
TeamDman <TeamDman9201@gmail.com>
2019-05-05 23:10:32 -0400

    Create generic ListenerTask

--- app/tasks/logger.ts
+++ app/tasks/logger.ts
@@ -1,9 +1,14 @@
-import {Client, Message, MessageAttachment, MessageEmbed, RichEmbed} from 'discord.js';
-import {MessageContent, Task}                                        from '../index';
-import * as logger                                                   from '../logger';
+import {Message, MessageAttachment, MessageEmbed} from 'discord.js';
+import * as logger                                from '../logger';
+import {ListenerTask}                             from '../tasks';
+import {Task}                                     from '../index';
 
-const listeners =  {
-    'messageDelete': (message:Message) => {
+function getInfo(message: Message) {
+    return `${message.guild}\t#${'name' in message.channel && message.channel.name || 'UNKNOWN CHANNEL'}\t<@${message.author.id}> (${message.author.tag})`;
+}
+
+export default new ListenerTask('Logger', {
+    'messageDelete': (message: Message) => {
         if (message.author.bot) return;
         logger.info(`!!! ${getInfo(message)}\t${message.content}`);
         message.embeds.forEach((embed: MessageEmbed) => logger.info(`!!! ${getInfo(message)}\t${embed.type}embed:${embed.url}`));
@@ -15,40 +20,6 @@ const listeners =  {
         if (original.content === updated.content) return;
         const line = `*** ${getInfo(original)}\t`;
         logger.info(`${line}${original.content}`);
-        logger.info(`${line.replace(/[^\t]/g,' ')}${updated.content}`);
+        logger.info(`${line.replace(/[^\t]/g, ' ')}${updated.content}`);
     }
-};
-let running:boolean = false;
-
-function getInfo(message:Message) {
-    return `${message.guild}\t#${'name' in message.channel && message.channel.name || 'UNKNOWN CHANNEL'}\t<@${message.author.id}> (${message.author.tag})`;
-}
-
-function start(client: Client): MessageContent {
-    if (running)
-        return new RichEmbed().setColor('ORANGE').setDescription('Logger task is already running.');
-    for (let key of Object.keys(listeners)) {
-        client.addListener(key, listeners[key]);
-    }
-    running = true;
-
-    return new RichEmbed().setColor('GREEN').setDescription('Logger task has been started.');
-}
-
-function stop(client: Client): MessageContent {
-    if (!running)
-        return new RichEmbed().setColor('ORANGE').setDescription('Logger task is not running.');
-    for (let key of Object.keys(listeners)) {
-        client.removeListener(key, listeners[key]);
-    }
-    running = false;
-    return new RichEmbed().setColor('GREEN').setDescription('Logger task has been stopped.');
-}
-
-export default {
-    name: 'Logger',
-    allowConcurrent: false,
-    autoStart: true,
-    start,
-    stop
-} as Task
+}) as Task;
