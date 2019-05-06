0af854e49c4a9a53feb9e405ef47364f588e7ff5
TeamDman <TeamDman9201@gmail.com>
2019-05-05 22:48:03 -0400

    Update config wrapping

--- app/config.ts
+++ app/config.ts
@@ -4,12 +4,12 @@ const config = require('../config.json');
 
 // https://stackoverflow.com/a/51413778/11141271
 
-export default new Proxy(config,{
+const wrap: any = (obj: any) => new Proxy(obj,{
     get(target: {}, p: string | number | symbol, receiver: any): any {
         let result = Reflect.get(target, p, receiver);
         if (!target.hasOwnProperty(p))
             throw new Error(`Unknown config value '${String(p)}'.`);
-        return result;
+        return typeof result === 'object' ? wrap(result) : result;
     },
     set(target: {}, p: string | number | symbol, value: any, receiver: any): boolean {
         //return Reflect.set(target, p, value, receiver);
@@ -17,3 +17,5 @@ export default new Proxy(config,{
         return false;
     }
 });
+
+export default wrap(config);
