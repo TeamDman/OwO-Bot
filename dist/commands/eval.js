52d92b51e6228bd3a8bdf2c421d804131702e91a
TeamDman <TeamDman9201@gmail.com>
2019-05-12 11:38:32 -0400

    Fix eval missing perm

--- dist/commands/eval.js
+++ dist/commands/eval.js
@@ -22,6 +22,7 @@ exports.default = {
     name: 'Evaluate',
     commands: ['eval', 'exec'],
     description: 'Evaluates a given javascript statement',
+    permissions: ['MANAGE_BOT'],
     parameters: [{
             name: 'Code',
             type: 'STRING'
