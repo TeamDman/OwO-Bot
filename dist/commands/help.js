fdb374d54b6c02b3edc5524f4561f4de1676f277
TeamDman <TeamDman9201@gmail.com>
2019-05-12 14:07:46 -0400

    Update help

--- dist/commands/help.js
+++ dist/commands/help.js
@@ -11,56 +11,44 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const discord_js_1 = require("discord.js");
 const commands_1 = require("../commands");
 const config_1 = require("../config");
-const invoke = (message, route, args) => __awaiter(this, void 0, void 0, function* () {
-    switch (route) {
-        case '':
-            return new discord_js_1.RichEmbed()
-                .setTitle('Commands')
-                .setDescription(commands_1.getCommands().map((c) => `${c.name}: ${c.description}`).join('\n'));
-        case 'info':
-            args[0] = args[0].toLowerCase();
-            let command = commands_1.getCommands().find(c => c.name.toLowerCase() == args[0] || c.commands.some(com => com.toLowerCase() == args[0]));
-            if (command === null)
-                return new discord_js_1.RichEmbed().setColor('ORANGE').setDescription(`Could not find command named ${args[0]}.`);
-            let rtn = new discord_js_1.RichEmbed().setTitle(command.name)
-                .addField('Description', command.description, true)
-                .addField('Aliases', command.commands.join(', '), true);
-            let buildParamString = (params) => {
-                return params.filter(p => p.name && p.description)
-                    .map(p => `${p.name}: ${p.description || ''}`)
-                    .join('\n');
-            };
-            if ('parameters' in command) {
-                rtn.addField('Usage', `${config_1.default.bot.prefix} ${command.commands[0]} ${command.parameters.map(p => `<${p.type} ${p.name}>`).join(' ')}`
-                    + '\n\n' + buildParamString(command.parameters));
-            }
-            if ('routes' in command) {
-                for (let route of Object.keys(command.routes)) {
-                    rtn.addField(command.routes[route].name || route || '*', `${config_1.default.bot.prefix} ${command.commands[0]} ${route} ${(command.routes[route].parameters || []).map(p => `<${p.type} ${p.name}>`).join(' ')}`
-                        + `\n${command.routes[route].description}\n` + buildParamString(command.routes[route].parameters || []));
-                }
-            }
-            return rtn;
+const invoke = (message, args) => __awaiter(this, void 0, void 0, function* () {
+    if (args[0] === '')
+        return new discord_js_1.RichEmbed()
+            .setTitle('Commands')
+            .setDescription(commands_1.getCommands().map((c) => `${c.name}: ${c.description}`).join('\n'));
+    args[0] = args[0].toLowerCase();
+    let command = commands_1.getCommands().find(c => c.name.toLowerCase() == args[0] || c.commands.some(com => com.toLowerCase() == args[0]));
+    if (command === null)
+        return new discord_js_1.RichEmbed().setColor('ORANGE').setDescription(`Could not find command named ${args[0]}.`);
+    let rtn = new discord_js_1.RichEmbed().setTitle(command.name)
+        .addField('Description', command.description, true)
+        .addField('Aliases', command.commands.join(', '), true);
+    let buildParamString = (params) => {
+        return params.filter(p => p.name && p.description)
+            .map(p => `${p.name}: ${p.description || ''}`)
+            .join('\n');
+    };
+    if ('parameters' in command) {
+        rtn.addField('Usage', `${config_1.default.bot.prefix} ${command.commands[0]} ${command.parameters.map(p => `<${p.type} ${p.name}>`).join(' ')}`
+            + '\n\n' + buildParamString(command.parameters));
     }
+    if ('routes' in command) {
+        for (let route of Object.keys(command.routes)) {
+            rtn.addField(command.routes[route].name || route || '*', `${config_1.default.bot.prefix} ${command.commands[0]} ${route} ${(command.routes[route].parameters || []).map(p => `<${p.type} ${p.name}>`).join(' ')}`
+                + `\n${command.routes[route].description}\n` + buildParamString(command.routes[route].parameters || []));
+        }
+    }
+    return rtn;
 });
 exports.default = {
     name: 'Help',
     commands: ['help', 'info', 'cmds', 'commands'],
     description: 'Displays command information.',
-    routes: {
-        '': {
-            name: 'List Commands',
-            description: 'Lists available commands.'
-        },
-        'info': {
-            name: 'Command Info',
-            description: 'Displays information for a specific command.',
-            parameters: [{
-                    name: 'Command Name',
-                    type: 'STRING'
-                }]
-        }
-    },
+    parameters: [{
+            name: 'Command Name',
+            description: 'The name of the command to display info for. Empty to list all.',
+            type: 'STRING'
+        }],
     executor: invoke
 };
 //# sourceMappingURL=help.js.map
