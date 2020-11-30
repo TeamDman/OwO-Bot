5f79ec8a4f40104994ab03206eb181c2bd0b2448
TeamDman <TeamDman9201@gmail.com>
2020-11-29 23:28:41 -0500

    Add most code

---
+++ app/src/index.d.ts
@@ -0,0 +1,25 @@
+import { Cookie } from "set-cookie-parser";
+
+interface User {
+    name: string;
+    creds: UserCredential;
+}
+
+interface UserTimeSlotInfo {
+    reservations: TimeSlot[],
+    availabilities: TimeSlot[],
+}
+
+type UserCookie = Cookie[];
+
+type TimeSlot = {
+    id: string;
+    club?: string;
+    date?: string;
+    time?: string;
+};
+
+interface UserCredential {
+    emailaddress: string;
+    password: string;
+}
---
+++ app/src/users.example.ts
@@ -0,0 +1,11 @@
+import { User } from "./index.d";
+
+export const users: User[] = [
+    {
+        name: "John Doe",
+        creds: {
+            emailaddress: "john@a.com",
+            password: "hunter2",
+        }
+    }
+];
